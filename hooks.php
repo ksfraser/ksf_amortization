eebd6246b85517ce91edcb44c584578db07a7392
root@fhsws002.ksfraser.com <root@fhsws002.ksfraser.com>
2025-08-22 09:35:30 -0600

    Trying to make run on FA
     Changes to be committed:
    	new file:   _init/config
    	new file:   hooks.php

---
+++ hooks.php
@@ -0,0 +1,57 @@
+<?php
+
+define( 'MENU_AMORTIZATION', 'menu_amortization' );
+
+class hooks_ksf_amortization extends hooks {
+    var $module_name = 'ksf_amortization'; 
+
+    /*
+    * Install additonal menu options provided by module
+    */
+
+    function install_options($app) {
+	global $path_to_root;
+
+
+	switch($app->id) {
+	    case 'GL':
+		$app->add_lapp_function(3, _("Amortization Generic"),
+			$path_to_root."/modules/".$this->module_name."/src/Ksfraser/Amortizations/controller.php", 'SA_CUSTOMER', MENU_AMORTIZATION);
+		$app->add_lapp_function(3, _("Amortization Admin"),
+			$path_to_root."/modules/".$this->module_name."/src/Ksfraser/Amortizations/controller.php?action=admin", 'SA_CUSTOMER', MENU_AMORTIZATION);
+		$app->add_lapp_function(3, _("Amortization Create"),
+			$path_to_root."/modules/".$this->module_name."/src/Ksfraser/Amortizations/controller.php?action=create", 'SA_CUSTOMER', MENU_AMORTIZATION);
+		$app->add_lapp_function(3, _("Amortization Reports"),
+			$path_to_root."/modules/".$this->module_name."/src/Ksfraser/Amortizations/controller.php?action=report", 'SA_CUSTOMER', MENU_AMORTIZATION);
+		break;
+	}
+    }
+
+
+    function activate_extension($company, $check_only=true) {
+	$updates = array( 'update.sql' => array($this->module_name) );
+	return $this->update_databases($company, $updates, $check_only);
+	return true;
+    }
+
+    //this is required to cancel bank transactions when a voiding operation occurs
+	//@todo refactor to use my eventloop functions
+    function db_prevoid($trans_type, $trans_no) {
+/**
+	    //SET status=0
+	$sql = "
+	    UPDATE ".TB_PREF."bi_transactions
+	    SET status=0, fa_trans_no=0, fa_trans_type=0, created=0, matched=0, g_partner='', g_option=''
+	    WHERE
+		fa_trans_no=".db_escape($trans_no)." AND
+		fa_trans_type=".db_escape($trans_type)." AND
+		status = 1";
+	display_notification($sql);
+	db_query($sql, 'Could not void transaction');
+*/
+
+    }
+
+
+}
+?>
